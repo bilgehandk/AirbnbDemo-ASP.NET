5bb14bd84f0e3690e3a233472b795f7cb7dc7cab
bmathews42 <brocmathews@mail.weber.edu>
2024-08-13 11:33:08 +0300

    adding cache

--- Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4012edeb8d58a73f8623f4f3aa8a1e89e464fe9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbb8d077d2613a09c02c837d7a58b2369b93c1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
