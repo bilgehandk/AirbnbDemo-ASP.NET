106db512e1f15f99d27506b5cb88f274945874d2
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2024-08-15 05:05:27 +0300

    OnGet in Property Detail, Add icon library and edit path in Index to Property Detail

---
+++ AirbnbDemo/Pages/PropertyDetails.cshtml.cs
@@ -0,0 +1,41 @@
+using Infrastructure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
+
+namespace CBTDWeb.Pages
+{
+    public class PropertyDetailsModel : PageModel
+    {
+        private readonly UnitOfWork _unitOfWork;
+        public PropertyInfo objProperty { get; set; }
+        [BindProperty]
+        public int hostCount { get; set; }
+        public ApplicationUser hostUser { get; set; }
+
+        public PropertyDetailsModel(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            objProperty = new PropertyInfo();
+            hostUser = new ApplicationUser();
+        }
+
+        public IActionResult OnGet(int? id)
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity; // get the user object that is returned when logged >> might be a list of users
+            var claim = claimsIdentity?.FindFirst(ClaimTypes.NameIdentifier); //grab the logged in user, normally at idx 0
+            TempData["UserLoggedIn"] = claim;
+
+
+            //FOR OUTER JOIN , CATEGORY.MANUFACTUER, STUDENT = category left join manufacturer and join student
+            objProperty = _unitOfWork.Property.Get(p => p.Id == id, includes: "Amenity,Fee");
+            hostUser = _unitOfWork.ApplicationUser.Get(u => u.Id == objProperty.OwnerId);
+            // the p here stored the result of the sql querry
+            // SELECT * FROM PRODUCTS P
+            // JOIN CATEGORIES C ON C.PRODUCTID = P.PRODUCTID
+            // JOIN MANUFACTURERS M ON M.PRODUCTID = P.PRODUCTID
+            // WHERE P.ID = @ID
+            return Page();
+        }
+    }
+}
